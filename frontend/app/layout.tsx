b1b28b0c19b54fae1ca7452f286c1bcd6f31af55
JoonhoeKim <26rote@gmail.com>
2024-08-17 19:32:00 +0900

    🤯architecture: 스택 재선택

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Noto_Sans_KR, Nanum_Myeongjo } from "next/font/google";
+import "./globals.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: [ '100', '400', '700', '900'],
+  variable: '--font-noto-sans-kr',
+  display: 'swap'
+
+})
+
+const nanumMyeongjo = Nanum_Myeongjo({
+  subsets: ["latin"],
+  weight: [ '400', '700',],
+  variable: '--font-nanum-myeongjo',
+  display: 'swap'
+})
+
+export const metadata: Metadata = {
+  title: "blllg",
+  description: "blllg is self-hostable web app inspired by obsidian.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${notoSansKr.variable} ${nanumMyeongjo.variable}`}>{children}</body>
+    </html>
+  );
+}
