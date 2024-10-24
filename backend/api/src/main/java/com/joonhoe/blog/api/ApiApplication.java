4a334035bb56a8642380ca851068efba4bf45247
joonhoekim <26rote@gmail.com>
2024-10-24 12:38:05 +0000

    applying Spring Security

--- backend/api/src/main/java/com/joonhoe/blog/api/ApiApplication.java
+++ backend/api/src/main/java/com/joonhoe/blog/api/ApiApplication.java
@@ -2,7 +2,10 @@ package com.joonhoe.blog.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
@@ -13,4 +16,8 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 }
